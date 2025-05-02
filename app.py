de628b3b7ebd8a4eae9bd3ddd9859b7e5746db9e
Youssef amr <amry14003@gmail.com>
2025-05-02 08:12:12 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import traceback
-from flask import Flask, request, jsonify, render_template, make_response
+from flask import Flask, request, jsonify, render_template
 import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 from werkzeug.utils import secure_filename
@@ -61,35 +60,19 @@ def share_file(filename):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    filename = secure_filename(file.filename)
     try:
-        if 'file' not in request.files:
-            return make_response(jsonify({"error": "No file part"}), 400)
-        
-        file = request.files['file']
-        if not file or file.filename == '':
-            return make_response(jsonify({"error": "No selected file"}), 400)
-        
-        filename = secure_filename(file.filename)
-        if not filename:
-            return make_response(jsonify({"error": "Invalid filename"}), 400)
+        s3.upload_fileobj(file, BUCKET_NAME, filename)
+        return jsonify({"message": "File uploaded successfully"}), 200
+    except (BotoCoreError, ClientError) as e:
+        return jsonify({"error": str(e)}), 500
 
-        s3.upload_file(
-            file,
-            BUCKET_NAME,
-            filename,
-            ExtraArgs={'ContentType': file.content_type}
-        )
-        
-        return make_response(jsonify({
-            "success": True,
-            "message": "File uploaded",
-            "filename": filename
-        }), 200)
-
-    except Exception as e:
-        return make_response(jsonify({
-            "error": str(e),
-            "stacktrace": traceback.format_exc()
-        }), 500)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)

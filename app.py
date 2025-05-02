137fc1cd4110be5b9a4699ab22b6ff55677a9248
Youssef amr <amry14003@gmail.com>
2025-05-02 05:58:24 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,63 +8,7 @@ BUCKET_NAME = 'my-app-bucket-afhoasihgoiahsgd035526asgtqt4625w'
 
 @app.route('/')
 def home():
-    return render_template("logIn.html")
-
-@app.route('/signUp.html')
-def signup():
-    return render_template("signUp.html")
-
-@app.route('/logIn.html')
-def logInpage():
-    return render_template("logIn.html")
-
-@app.route('/MainPage.html')
-def main():
     return render_template("MainPage.html")
-
-@app.route('/login')
-def logIn():
-    data = request.json
-    username = data['usernaem']
-    password = data['password']
-    folder_key = f'{username}/'
-    password_key = f'{username}/password.txt'
-
-
-    response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=folder_key)
-    if 'Contents' in response:
-        try:
-            stored_obj = s3.get_object(Bucket=BUCKET_NAME, Key=password_key)
-            stored_password = stored_obj['Body'].read().decode('utf-8')
-            if stored_password == password:
-                return render_template('MainPage.html')
-            else:
-                return jsonify({'authenticated': False}), 401
-        except s3.exceptions.NoSuchKey:
-            return jsonify({'authenticated': False}), 404
-    else:
-        return jsonify({'error': 'no User with this name'}), 401
-
-
-
-@app.route('/SignUp')
-def signUp():
-    data = request.json
-    username = data['username']
-    password= data['password']
-
-    folder_key=f'{username}/'
-    password_key = f'{username}/password.txt'
-
-    UserFolder = s3.put_object(Bucket=BUCKET_NAME, Key=folder_key)
-    UserPassword = s3.put_object(Bucket=BUCKET_NAME, Key=password_key, Body=password)
-
-    if UserPassword['ResponseMetadata']['HTTPStatusCode'] == 200 or UserFolder['ResponseMetadata']['HTTPStatusCode'] == 200:
-        return render_template('logIn.html')
-    else:
-        return jsonify({'SignedUp': False}), 401
-
-
 
 @app.route('/list_files')
 def list_files():

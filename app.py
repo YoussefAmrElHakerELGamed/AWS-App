480421681f6a2a596c302edf8a91a3128a66acea
Youssef amr <amry14003@gmail.com>
2025-05-02 06:44:28 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 s3 = boto3.client('s3')
@@ -10,67 +12,65 @@ BUCKET_NAME = 'my-app-bucket-afhoasihgoiahsgd035526asgtqt4625w'
 def home():
     return render_template("MainPage.html")
 
-@app.route('/list_files')
+@app.route('/list_files', methods=['GET'])
 def list_files():
     try:
         response = s3.list_objects_v2(Bucket=BUCKET_NAME)
-        
+
         if 'Contents' not in response:
-            return jsonify({"message": "No files found"}), 200
-        
-        files_details = []
-        for obj in response['Contents']:
-            file_details = {
+            return jsonify({"files": []}), 200
+
+        files = [
+            {
                 "filename": obj['Key'],
                 "last_modified": obj['LastModified'].strftime('%Y-%m-%d %H:%M:%S'),
                 "size": obj['Size'],
                 "storage_class": obj['StorageClass']
             }
-            files_details.append(file_details)
-        
-        return jsonify({"files": files_details}), 200
-    except Exception as e:
-        return jsonify({"error": "no files found"}), 500
+            for obj in response['Contents']
+        ]
 
+        return jsonify({"files": files}), 200
 
+    except (BotoCoreError, ClientError) as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/delete/<filename>')
+@app.route('/delete/<filename>', methods=['GET'])
 def delete_file(filename):
     try:
         s3.delete_object(Bucket=BUCKET_NAME, Key=filename)
         return jsonify({"message": f"File {filename} deleted successfully"}), 200
-    except Exception as e:
+    except (BotoCoreError, ClientError) as e:
         return jsonify({"error": str(e)}), 500
 
-
-@app.route('/share/<filename>')
+@app.route('/share/<filename>', methods=['GET'])
 def share_file(filename):
     try:
-        url = s3.generate_presigned_url('get_object',
-                                               Params={'Bucket': BUCKET_NAME, 'Key': filename},
-                                               ExpiresIn=3600)
+        url = s3.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': BUCKET_NAME, 'Key': filename},
+            ExpiresIn=3600  # 1 hour
+        )
         return jsonify({"url": url}), 200
-    except Exception as e:
+    except (BotoCoreError, ClientError) as e:
         return jsonify({"error": str(e)}), 500
 
-    
-@app.route('/upload')
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return jsonify({"error": "No file part"}), 400
+        return jsonify({"error": "No file part in the request"}), 400
+
     file = request.files['file']
-    
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
-    
-    filename = file.filename
-    
+
+    filename = secure_filename(file.filename)
+
     try:
         s3.upload_fileobj(file, BUCKET_NAME, filename)
         return jsonify({"message": "File uploaded successfully"}), 200
-    except Exception as e:
+    except (BotoCoreError, ClientError) as e:
         return jsonify({"error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
